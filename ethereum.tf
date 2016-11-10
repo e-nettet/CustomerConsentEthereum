f352307760622ec77b69e7e004699598ddb81dbf
Linas Zvirblis <lzv@trifork.com>
2016-11-10 12:00:51 +0100

    Added miner configuration for Ethereum.

--- ethereum.tf
+++ ethereum.tf
@@ -2,6 +2,14 @@ variable "docker_host" {
     default = "unix:///var/run/docker.sock"
 }
 
+variable "bootnode_enode" {
+    default = "53216148b33a67390c2c76a41e5b59af74f5842d2f983d17af66874d007f1d2b62efe6f73872a77a7799ab5b0a6fb4318416c2b8d3b6e7f665676629e2e45da0"
+}
+
+variable "miner_etherbase" {
+    default = "0x0000000000000000000000000000000000000001"
+}
+
 provider "docker" {
     host = "${var.docker_host}"
 }
@@ -33,7 +41,7 @@ resource "docker_container" "ethereum_node" {
         "${docker_container.ethereum_bootnode.name}:bootnode"
     ]
     command = [
-        "--bootnodes=enode://53216148b33a67390c2c76a41e5b59af74f5842d2f983d17af66874d007f1d2b62efe6f73872a77a7799ab5b0a6fb4318416c2b8d3b6e7f665676629e2e45da0@${docker_container.ethereum_bootnode.ip_address}:30301",
+        "--bootnodes=enode://${var.bootnode_enode}@${docker_container.ethereum_bootnode.ip_address}:30301",
         "--fast",
         "--lightkdf",
         "--rpc",
@@ -52,6 +60,28 @@ resource "docker_container" "ethereum_node" {
         "node0",
         "--wsport",
         "8546"
+    ]
+    must_run = true
+    restart = "no"
+}
+
+resource "docker_container" "ethereum_miner" {
+    image = "${docker_image.ethereum_node.latest}"
+    name = "ethereum-miner"
+    hostname = "miner"
+    depends_on = [
+        "docker_container.ethereum_bootnode"
+    ]
+    links = [
+        "${docker_container.ethereum_bootnode.name}:bootnode"
+    ]
+    command = [
+        "--bootnodes=enode://${var.bootnode_enode}@${docker_container.ethereum_bootnode.ip_address}:30301",
+        "--fast",
+        "--lightkdf",
+        "--mine",
+        "--minerthreads=1",
+        "--etherbase=${var.miner_etherbase}"
     ]
     must_run = true
     restart = "no"
