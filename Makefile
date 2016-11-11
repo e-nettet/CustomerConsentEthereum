2b61598407f2397d536b4d22e3da63a41cd422b2
Rikke Willer <rwi@trifork.com>
2016-11-11 11:24:48 +0100

    Docker images and terraform configuration for Ethereum network monitoring tool

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: ethereum-base ethereum-node ethereum-bootnode
+all: ethereum-base ethereum-node ethereum-bootnode ethereum-netstats
 nodes: ethereum-node ethereum-bootnode
 ethereum-base:
 	docker build -t ethereum-base base
@@ -6,3 +6,6 @@ ethereum-node: ethereum-base
 	docker build -t ethereum-node node
 ethereum-bootnode: ethereum-node
 	docker build -t ethereum-bootnode bootnode
+ethereum-netstats: ethereum-netstats
+	docker build -t ethereum-netstats eth-netstats
+	docker build -t ethereum-netstats-api eth-net-intelligence-api
--- ethereum.tf
+++ ethereum.tf
@@ -2,6 +2,10 @@ variable "docker_host" {
     default = "unix:///var/run/docker.sock"
 }
 
+variable "netstat_secret" {
+    default = "fc3790e391e058a04d6a81aac40e1e51adb675f85a2e0d5c9c95477b22836185ffe5761c4c21f049cdb2b579ff615935"
+}
+
 provider "docker" {
     host = "${var.docker_host}"
 }
@@ -12,6 +16,14 @@ resource "docker_image" "ethereum_node" {
 
 resource "docker_image" "ethereum_bootnode" {
     name = "ethereum-bootnode:latest"
+}
+
+resource "docker_image" "ethereum_netstats" {
+    name = "ethereum-netstats:latest"
+}
+
+resource "docker_image" "ethereum_netstats_api" {
+    name = "ethereum-netstats-api:latest"
 }
 
 resource "docker_container" "ethereum_node" {
@@ -70,4 +82,29 @@ resource "docker_container" "ethereum_bootnode" {
     ]
     must_run = true
     restart = "no"
+}
+
+resource "docker_container" "ethereum_netstats" {
+    image = "${docker_image.ethereum_netstats.latest}"
+    name = "ethereum-netstats"
+    ports {
+        internal = 3000
+        external = 3000
+    }
+    env = [
+      "WS_SECRET=${var.netstat_secret}"
+    ]
+}
+
+resource "docker_container" "ethereum_netstats_api" {
+    image = "${docker_image.ethereum_netstats_api.latest}"
+    name = "ethereum-netstats-api"
+    env = [
+      "WS_SECRET=${var.netstat_secret}"
+    ]
+    links = [
+      "${docker_container.ethereum_netstats.name}:netstats",
+      "${docker_container.ethereum_node.name}:node0",
+      "${docker_container.ethereum_bootnode.name}:node1"
+    ]
 }
