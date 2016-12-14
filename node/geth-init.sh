52436241afe6668fd3bae4c66b39564f6aa82c56
Linas Zvirblis <lzv@trifork.com>
2016-12-14 10:45:06 +0100

    Hardcoded static nodes for fast initial connection.

--- node/geth-init.sh
+++ node/geth-init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-[ -d /root/.ethereum/geth ] || $1 init /etc/ethereum/genesis.json
+[ -d /root/.ethereum/geth ] || $1 init /etc/ethereum/genesis.json && cp /etc/ethereum/static-nodes.json /root/.ethereum/static-nodes.json && cp /etc/ethereum/trusted-nodes.json /root/.ethereum/trusted-nodes.json
 eval "$@"
