9c3f6bc3541ffa2adb05635943839bea7c94139b
Linas Zvirblis <lzv@trifork.com>
2017-01-12 14:05:00 +0100

    Open port for EthExplorer.

--- terraform/modules/aws/aws.tf
+++ terraform/modules/aws/aws.tf
@@ -166,6 +166,16 @@ resource "aws_security_group" "consent_security_group" {
         ]
     }
     ingress {
+        from_port = 8000
+        to_port = 8000
+        protocol = "tcp"
+        self = true
+        cidr_blocks = [
+            "${split(",", var.consent_cidr_blocks)}",
+            "${aws_eip.consent_ip.public_ip}/32"
+        ]
+    }
+    ingress {
         from_port = 30301
         to_port = 30309
         protocol = "tcp"
